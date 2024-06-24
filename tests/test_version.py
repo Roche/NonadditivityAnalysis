151c3b0ce84747d92d89fb5b4757621ba94767eb
Niels Maeder <maedern@student.ethz.ch>
2024-06-24 17:47:24 +0200

    test correct package

--- tests/test_version.py
+++ tests/test_version.py
@@ -8,4 +8,4 @@ def test_version() -> None:
     """Test that package has version and it is not None."""
     assert hasattr(nonadditivity, "__version__")
     assert nonadditivity.__version__ is not None
-    assert nonadditivity.__version__ == version("roche-nonadditivity")
+    assert nonadditivity.__version__ == version("nonadditivity")
